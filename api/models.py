fa00c60feb38b552f4f3bb1f7331af22f86a8e61
Jack <jackfan0305@gmail.com>
2023-11-10 21:43:18 -0500

    Setup serializer, room views, and created API endpoint

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import string
+import random
+
+#Generates random and unique code for each room
+def generate_uninque_code():
+    length = 6
+    while True:
+        code  =  ''.join(random.choices(string.ascii_uppercase, k = length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 
 # Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=8,  default="", unique=True) #code for each room
+    host = models.CharField(max_length=50, unique=True) #room host
+    guest_can_pause = models.BooleanField(null=False, default = False) 
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True) #Time of creation
+
+    
