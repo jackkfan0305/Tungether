b2382ffbcb02ecd4c5e50f0f4ef30b627cf49ba8
Jack <jackfan0305@gmail.com>
2023-11-11 22:30:02 -0500

    Added a create room view with a post request, and migrated the change

--- api/models.py
+++ api/models.py
@@ -13,7 +13,7 @@ def generate_uninque_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8,  default="", unique=True) #code for each room
+    code = models.CharField(max_length=8,  default=generate_uninque_code, unique=True) #code for each room
     host = models.CharField(max_length=50, unique=True) #room host
     guest_can_pause = models.BooleanField(null=False, default = False) 
     votes_to_skip = models.IntegerField(null=False, default=1)
