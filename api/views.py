97f3a3dba747eb6306d47466bb66b1551d73a627
Jack <junwei.fan@mail.utoronto.ca>
2023-12-24 14:23:13 -0500

    fininshed roomJoiPage

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
@@ -26,6 +27,23 @@ class GetRoom(APIView):
             return Response({'Room Not Found':'Invalid Room Code'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'Bad Request': 'Code parameter not found in request'}, status = status.HTTP_400_BAD_REQUEST)
 
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create() #Creating a new session
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'Message': 'Room Joined!'}, status=status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"Bad Request": "Invalid post data, did not find a code key"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CreateRoomView(generics.CreateAPIView):
     serializer_class = CreateRoomSerializer
@@ -45,9 +63,21 @@ class CreateRoomView(generics.CreateAPIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code']= room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else: #Creating new room
                 room = Room(host=host, guest_can_pause = guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
+                self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST) #invalid serializer
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create() #Creating a new session
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component, useState, useCallback, useEffect } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link, useNavigate} from "react-router-dom";
 
-export default class RoomJoinPage extends Component{
-    constructor(props){
-        super(props);
-    }
+function RoomJoinPage() {
     
-    render(){
-        return <p>This is the join roomm page</p>
-    }
+  const initialState = {
+    roomCode: "",
+    error: "",
+  };
+  const navigate = useNavigate();
+  const [isValid, setIsValid] = useState(false);
+  const [roomData, setRoomData] = useState(initialState);
+
+  const sendRequest = useCallback(() => {
+    const requestOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            code: roomData.roomCode
+        })
+
+    };
+    fetch('/api/join-room', requestOptions)
+    .then((response)=>{
+        if (response.ok){
+           navigate(`/room/${roomData.roomCode}`)
+           if (setIsValid.current){
+            setIsValid(false);
+           }
+             
+        }
+        else {
+            setIsValid(true);
+            setRoomData({...roomData, error: "Room Not Found"})
+        }
+    }).catch((error)=>{
+        console.log(error)
+    });
+  })
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4" component="h4">
+          Join a Room
+        </Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <TextField
+          error={isValid} //expects a boolean, need to fix 
+          label="Code" 
+          placeholder="Enter a Room Code"
+          value={roomData.roomCode}
+          helperText={roomData.error}
+          variant="outlined"
+          onChange = {(ev) => {
+            setRoomData({...roomData, roomCode: ev.target.value})
+          }}
+        />
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color = "primary" commponent = {Link} onClick = {sendRequest}>
+            Enter Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button variant="contained" color = "secondary" to="/" component = {Link}>
+            Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
+
+export default RoomJoinPage;
