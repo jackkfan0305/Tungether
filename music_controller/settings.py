234824bc0fba2022996503cf6f4f0cd5bd1ac978
Jack <junwei.fan@mail.utoronto.ca>
2024-01-02 12:14:41 -0500

    updated hosts

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rmx0i@@ux0apuq%js_dur^p)5+gd+4g**fvvt4$43cab&3=+1h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 #'.vercel.app', '127.0.0.1'
 
 
